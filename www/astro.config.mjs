6777382089577bcda9dafbf0aee2040c8bd8de53
Parth Sidpara <parthsidpara2005@gmail.com>
2025-07-23 22:34:46 +0530

    piosk website

---
+++ www/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://debloper.github.io',
+    base: 'www',
+});
