1125fbca5a1af90eade039fa883c33bf6bba6fa2
Parth Sidpara <parthsidpara2005@gmail.com>
2025-07-29 23:25:13 +0530

    add workflow and fix base

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
+// @ts-ignore
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://debloper.github.io',
-    base: 'www',
+    base: '/piosk',
 });
