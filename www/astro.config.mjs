87df8425ecc3fe932a0598a450ec110d8b1d38fd
Parth Sidpara <parthsidpara2005@gmail.com>
2025-07-31 17:57:02 +0530

    simplify workflow and change the URL

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://debloper.github.io',
+    site: 'https://code.debs.io',
     base: '/piosk',
 });
