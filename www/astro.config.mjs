6aed3667aaf87073551968a2fe5e13775c1baca9
Parth <parthsidpara2005@gmail.com>
2025-12-17 00:03:47 +0530

    rename PiOSK to XiOSK in package, config, readme

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://code.debs.io',
-    base: '/piosk',
+    base: '/xiosk',
 });
